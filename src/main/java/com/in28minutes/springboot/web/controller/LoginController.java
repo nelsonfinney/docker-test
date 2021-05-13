429a02cfb7f7f5ddba5ed2ef938855f94b4d821d
M <923320@cognizant.com>
2021-05-13 11:36:39 +0530

    initial commit

---
+++ src/main/java/com/in28minutes/springboot/web/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.in28minutes.springboot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String showLoginPage(ModelMap model){
+		return "login";
+	}
+	
+
+	@RequestMapping(value="/hey", method = RequestMethod.GET)
+	public String showLoginPage(){
+		return "hey";
+	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String showWelcomePage(ModelMap model, @RequestParam String name){
+		model.put("name", name);
+		return "welcome";
+	}
+
+}
